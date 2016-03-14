afa614a0547c01eb55b919a57a99aa1572ff5e82
Koen <koen@koenvh.nl>
2016-03-14 23:18:31 +0100

    Edited it to let it load city names

--- ETS2 Log to Coordinates/Program.cs
+++ ETS2 Log to Coordinates/Program.cs
@@ -21,6 +21,53 @@ namespace ETS2_Log_to_Coordinates
 
         static void Main(string[] args)
         {
+            Dictionary<string, string> cityConversionTable = new Dictionary<string, string>();
+
+            string cityDirectory = ConfigurationManager.AppSettings["CityDirectory"];
+            string[] files = Directory.GetFiles(cityDirectory);
+            int numberOfCities = files.Length;
+
+            for (int i = 0; i < numberOfCities; i++)
+            {
+                StreamReader readCity = new StreamReader(files[i]);
+                string line;
+                //string output = read.ReadToEnd();
+                //string[] outputArray = output.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+                //read.Close();
+                try
+                {
+                    string cityName = "";
+                    string cityRealName = "";
+                    while ((line = readCity.ReadLine()) != null)
+                    {
+                        if (line.Trim().StartsWith("city_data: city."))
+                        {
+                            //int nameIndex = line.IndexOf("\"");
+                            //string cityName = line.Substring(nameIndex + 1,
+                            //    line.IndexOf("\"", nameIndex + 1) - nameIndex - 1);
+                            cityName = line.Replace("city_data: city.", "");
+                            if (cityName.Contains("{"))
+                            {
+                                cityName = cityName.Remove(cityName.IndexOf("{"));
+                            }
+                            cityName.Replace(" ", "");
+                        }
+                        if (line.TrimStart().StartsWith("city_name:"))
+                        {
+                            int nameIndex = line.IndexOf("\"");
+                            cityRealName = line.Substring(nameIndex + 1,
+                                line.IndexOf("\"", nameIndex + 1) - nameIndex - 1);
+                        }
+                    }
+                    cityConversionTable.Add(cityName, cityRealName);
+                    Console.WriteLine(cityName + " - " + cityRealName);
+                }
+                catch (Exception ex)
+                {
+                    Console.Write(ex.ToString());
+                }
+            }
+
             string inputFile = ConfigurationManager.AppSettings["InputFile"];
             StreamReader read = new StreamReader(inputFile);
             string output = read.ReadToEnd();
@@ -41,10 +88,20 @@ namespace ETS2_Log_to_Coordinates
                     !(lineContentArray[2] == previousX && lineContentArray[3] == previousY &&
                       lineContentArray[4] == previousZ))
                 {
-                    cities.citiesList.Add(lineContentArray);
-                    previousX = lineContentArray[2];
-                    previousY = lineContentArray[3];
-                    previousZ = lineContentArray[4];
+                    try
+                    {
+                        City city = new City();
+                        city.gameName = lineContentArray[0];
+                        city.realName = cityConversionTable[lineContentArray[0]];
+                        city.x = lineContentArray[2];
+                        city.y = lineContentArray[3];
+                        city.z = lineContentArray[4];
+                        cities.citiesList.Add(city);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
                 }
             }
             string jsonCitiesList = Newtonsoft.Json.JsonConvert.SerializeObject(cities, Formatting.Indented);
@@ -53,12 +110,25 @@ namespace ETS2_Log_to_Coordinates
             StreamWriter write = new StreamWriter(outputFile);
             write.Write(jsonCitiesList);
             write.Close();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("-- Done, have fun :-)");
             Console.ReadLine();
         }
     }
 
     class Cities
     {
-         public List<string[]> citiesList = new List<string[]>();
+        public List<City> citiesList = new List<City>();
+    }
+
+    class City
+    {
+        public string gameName = "";
+        public string realName = "";
+
+        public string x = "";
+        public string y = "";
+        public string z = "";
     }
 }
